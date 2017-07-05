8955cd932c43608b7511de60a679c383115ab904
George Cairns <george@automationlogic.com>
2017-07-05 17:36:12 +0100

    added restart always

--- start.sh
+++ start.sh
@@ -12,6 +12,7 @@ docker run -d \
 	-v "$SCRIPTPATH/license.txt":/etc/tower/license \
 	-v tower_certs:/certs \
 	-e TOWER_INIT_SCM_URL=https://github.com/dayglo/synergy-poc-setup \
+	--restart=always \
 	dayglo/dxc-ansible-tower:3.1.1.1
 
 docker logs -f tower
